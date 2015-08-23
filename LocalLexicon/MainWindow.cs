93ce7bde14abcc4e6b70b5504be0c8bce4d5648f
Chris Wright <dhasenan@gmail.com>
2015-08-23 23:51:33 +0000

    fix prior!

---
+++ LocalLexicon/MainWindow.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+using Gtk;
+using LocalLexicon;
+
+public partial class MainWindow: Gtk.Window
+{
+    ListStore wordStore;
+
+    public MainWindow()
+        : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        wordStore = new ListStore(typeof(string), typeof(string));
+        var lexColumn = new TreeViewColumn();
+        var cellRendererText = new CellRendererText();
+        lexColumn.PackStart(cellRendererText, true);
+        lexColumn.AddAttribute(cellRendererText, "text", 0);
+        wordList.AppendColumn(lexColumn);
+        wordList.Model = wordStore;
+    }
+
+    Language _lang;
+
+    public void ShowLang(Language lang)
+    {
+        _lang = lang;
+        wordStore.Clear();
+        foreach (var word in lang.Words)
+        {
+            wordStore.AppendValues(word.Lex, word.Id.ToString());
+        }
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void OnWordListCursorChanged(object sender, EventArgs e)
+    {
+        // This one fires when a person clicks a row
+        //throw new NotImplementedException();
+        TreeIter iter;
+        if (wordList.Selection.GetSelected(out iter))
+        {
+            var guidStr = (string) wordStore.GetValue(iter, 1);
+            var guid = Guid.Parse(guidStr);
+            var word = _lang.Words.Where(x => x.Id == guid).FirstOrDefault();
+            Console.WriteLine("found word {0}", word);
+            this.currentWord.ShowWord(word);
+        }
+    }
+
+    protected void OnNewActionActivated(object sender, EventArgs e)
+    {
+    }
+
+    protected void OnOpenActionActivated(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void OnSaveActionActivated(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void OnNewWordActionActivated(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void OnDeleteWordActionActivated(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected void OnLanguagePropertiesActionActivated(object sender, EventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ LocalLexicon/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Gtk;
+using Newtonsoft.Json;
+
+namespace LocalLexicon
+{
+    public class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            var lang = new Language();
+            lang.AlphabetOrder = "bpvfwdtðþsƹkghaāeēoui";
+            lang.Name = "Sturnan";
+            lang.Notes = "Here are language notes!";
+            lang.Words.Add(new Word {
+                Id = Guid.NewGuid(),
+                Lex = "pehur",
+                Gloss = "fire",
+                Definition = "Exothermic oxidation reactions resulting in the generation of visible light.\nAlso, hot."
+            });
+            lang.Words.Add(new Word {
+                Id = Guid.NewGuid(),
+                Lex = "ðivros",
+                Gloss = "beaver",
+                Definition = "Cute critters with slappy tails."
+            });
+            Console.WriteLine("{0}", JsonConvert.SerializeObject(lang, Formatting.Indented));
+            
+            Application.Init();
+            MainWindow win = new MainWindow();
+            win.ShowLang(lang);
+            win.Show();
+            Application.Run();
+        }
+    }
+}
